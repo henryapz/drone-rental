2550d9d83464a8d4c3acd85c471de8bacb05a163
Felipe Ospina <felipeospina21@hotmail.com>
2021-12-15 13:48:22 -0500

    add Layout comp

---
+++ drone-rental-frontend/src/components/Shared/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+
+const Layout = (props) => {
+  return (
+    <>
+      <header/>
+      <main>{props.children}</main>
+      <Footer/>
+    </>
+  );
+};
+
+export default Layout;
