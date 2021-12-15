24bc593d1b4a1ef75cdfcbbe0ff1c41fd88a0729
Felipe Ospina <felipeospina21@hotmail.com>
2021-12-15 13:28:28 -0500

    add theme.js config file

---
+++ drone-rental-frontend/src/assets/styles/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  pallete: {
+    bg: '#fffffe',
+    darkBg: 'd8eefe',
+    text: '#5f6c7b',
+    contrast: '#ef4565',
+    darkBlue: '#094067',
+    lightBlue: '#3da9fc',
+  },
+  typography: {
+    main: ['Montserrat', 'Roboto', 'sans-serif'],
+  },
+});
+
+export default theme;
