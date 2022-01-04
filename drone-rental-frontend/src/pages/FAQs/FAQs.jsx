42bc427a0ed0955c6c6de409f0ce27d5e8b8d0f3
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-01-04 12:42:20 -0500

    Adding more faqs

--- drone-rental-frontend/src/pages/FAQs/FAQs.jsx
+++ drone-rental-frontend/src/pages/FAQs/FAQs.jsx
@@ -18,21 +18,47 @@ function FAQs() {
   const data = [
     {
       id: 'panel1',
-      title: 'General settings',
+      title: '¿Cómo pago el alquiler de un dron?',
       description:
-        'Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id dignissim quam.',
+        'Complete el proceso de pedido como se describe arriba. Al finalizar la compra, ' +
+        'después de ingresar su información, se lo dirigirá al sitio web seguro de Paypal.',
     },
     {
       id: 'panel2',
-      title: 'Users',
+      title: '¿Cómo reservo un dron para fechas específicas?',
       description:
-        'Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.',
+        'Complete el proceso de pedido como se describe arriba y seleccione la fecha' +
+        ' de inicio y finalización de su alquiler. Su alquiler comienza cuando llega' +
+        ' a su ubicación (para envío) o cuando lo recoge (para recogida local). Su ' +
+        'alquiler finaliza cuando lo devuelve o lo deja. Nos pondremos en contacto' +
+        ' contigo si surge algún problema. El dron llegará al final del día que ' +
+        'especifique como fecha de inicio. El dron debe enviarse de regreso en o ' +
+        'antes de la fecha de finalización que especifique.',
     },
     {
       id: 'panel3',
-      title: 'Personal data',
+      title: '¿El período de alquiler incluye el tiempo de tránsito del envío?',
       description:
-        'Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.',
+        'No. El período de alquiler comienza el día en que recibe el dron y finaliza' +
+        ' el día en que lo devuelve. Por ejemplo, un alquiler de 3 días podría ' +
+        ' comenzar el viernes y terminar el lunes. El alquiler de un dron no puede' +
+        ' comenzar ni finalizar un domingo ni un feriado en Perú',
+    },
+    {
+      id: 'panel4',
+      title: '¿A dónde envían?',
+      description:
+        'Le enviaremos un dron de alquiler a cualquier lugar de los Perú y el envío rápido está disponible.',
+    },
+    {
+      id: 'panel5',
+      title: '¿Qué pasa si he realizado el pago y necesito cancelar?',
+      description:
+        'Intente notificarnos tan pronto como sepa que debe cancelar. Si el dron' +
+        ' se ha enviado, el alquiler no se puede cancelar. Si el dron aún no se ' +
+        'ha enviado, puede reprogramarlo para otro momento o puede cancelarlo. ' +
+        'Es posible que se aplique una tarifa de cancelación según la cantidad ' +
+        'de aviso que haya dado antes de la fecha de inicio.',
     },
   ];
 
