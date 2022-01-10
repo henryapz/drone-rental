88f6b40887a896263be6dbfa09c2696c848a3714
Felipe Ospina <felipeospina21@hotmail.com>
2022-01-10 16:13:34 -0500

    fix form layout issue

--- drone-rental-frontend/src/components/Shared/SignForm/SignForm.jsx
+++ drone-rental-frontend/src/components/Shared/SignForm/SignForm.jsx
@@ -12,27 +12,28 @@ import { PropTypes } from 'prop-types';
 function SignForm({ register }) {
   return (
     <Container>
-      <Stack spacing={1} justifyContent="center" alignItems="center" margin="1rem">
-        <Stack
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            // error
-            // helperText="Incorrect entry."
-            type="email"
-            id="sign-form-email"
-            label="Email"
-            placeholder="email@someemail.com"
-          />
+      <Stack
+        component="form"
+        spacing={1}
+        justifyContent="center"
+        alignItems="center"
+        margin="5rem auto"
+        sx={{
+          '& .MuiTextField-root': { width: '20rem' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          // error
+          // helperText="Incorrect entry."
+          type="email"
+          id="sign-form-email"
+          label="Email"
+          placeholder="email@someemail.com"
+        />
 
-          <TextField type="password" id="sign-form-passwd" label="Password" />
-        </Stack>
-
+        <TextField type="password" id="sign-form-passwd" label="Password" />
         {register ? (
           <>
             <TextField
