3fc5a674740b29c875debe8be4117960a744d9b3
Felipe Ospina <felipe.ospina@agileengine.com>
2022-03-16 17:48:39 -0500

    fix: Hero mas-width

--- drone-rental-frontend/src/components/Shared/Hero/Hero.jsx
+++ drone-rental-frontend/src/components/Shared/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import { PropTypes } from 'prop-types';
 
 function Hero({ url, alt }) {
   return (
-    <Card>
+    <Card sx={{ maxWidth: '1800px', margin: '0.2rem auto' }}>
       <CardMedia component="img" width="100%" height="500" image={url} alt={alt} />
     </Card>
   );
