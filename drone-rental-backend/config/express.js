a496b0c099c1572afa4e497b75fa45df6f9ce8a1
Felipe Ospina <felipe.ospina@agileengine.com>
2022-03-26 21:20:48 -0500

    update drones crud backend

--- drone-rental-backend/config/express.js
+++ drone-rental-backend/config/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 function config(app) {
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
+  app.use(bodyParser.json());
   app.use(morgan('dev'));
   app.use(cors());
 }
