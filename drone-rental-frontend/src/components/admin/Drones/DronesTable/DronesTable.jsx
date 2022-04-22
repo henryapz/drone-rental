03ee58607f68276ce93b575c1c3d79589b4bbd97
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-04-22 09:51:32 -0500

    Updated unused variables

--- drone-rental-frontend/src/components/admin/Drones/DronesTable/DronesTable.jsx
+++ drone-rental-frontend/src/components/admin/Drones/DronesTable/DronesTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import Table from '@mui/material/Table';
